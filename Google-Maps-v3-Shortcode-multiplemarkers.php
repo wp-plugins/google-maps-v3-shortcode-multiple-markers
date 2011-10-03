a39db86e928ee2cad35633818ff3eeefdfefcc52
soundwaves-productions <soundwaves-productions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-03 11:03:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/google-maps-v3-shortcode-multiple-markers/trunk@446647 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Google-Maps-v3-Shortcode-multiplemarkers.php
@@ -0,0 +1,124 @@
+<?php
+/*
+Plugin Name: Google Maps v3 Shortcode multiple Markers
+Description: This plugin allows you to add one or more maps to your page/post using shortcodes, even multiple markers per map! Kudos to yohman for his plugin: http://wordpress.org/extend/plugins/google-maps-v3-shortcode/
+Version: 1.0
+Author: soundwaves-productions
+*/
+
+// Add the google maps api to header
+add_action('wp_head', 'MultipleMarkerMap_header');
+
+function MultipleMarkerMap_header() {
+	?>
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+	<?php
+}
+
+// Main function to generate google map
+function MultipleMarkerMap_call($attr) {
+
+	// default atts
+	$attr = shortcode_atts(array(	
+									'lat'   => '0', 
+									'lon'    => '0',
+									'id' => 'map',
+									'z' => '1',
+									'w' => '400',
+									'h' => '300',
+									'maptype' => 'ROADMAP',
+									'marker' =>''								
+									), $attr);
+									
+	$returnme = '
+    <div id="' .$attr['id'] . '" style="width:' . $attr['w'] . 'px;height:' . $attr['h'] . 'px;border:1px solid gray;"></div><br>
+    <script type="text/javascript">
+    var infowindow = null;
+		var latlng = new google.maps.LatLng(' . $attr['lat'] . ', ' . $attr['lon'] . ');
+		var myOptions = {
+			zoom: ' . $attr['z'] . ',
+			center: latlng,
+			mapTypeId: google.maps.MapTypeId.' . $attr['maptype'] . '
+		};
+		var ' . $attr['id'] . ' = new google.maps.Map(document.getElementById("' . $attr['id'] . '"),
+		myOptions);
+		';
+   $returnme .=' var sites = [';
+		//marker: show if address is not specified
+		if ($attr['marker'] != '')
+		{
+			$markers = explode("|",$attr['marker']);
+			for($i = 0;$i < count($markers);$i ++)
+			{
+				$markerTmp=$markers[$i];
+				$marker= explode(",",$markerTmp);
+					if (count($marker)>3) { 
+	  				$markerTmp2 .='['.$marker[0].',' .$marker[1].',\'' . $marker[2] . '\',\'' . $marker[3] . '\'],';
+					} else {
+	  				$markerTmp2 .='['.$marker[0].',' .$marker[1].',\'' . $marker[2] . '\',null],';
+						}
+	  	}
+	  }
+	 $markerTmp2=substr ($markerTmp2,0,strlen ( $markerTmp2 )-1);
+	 $returnme .=$markerTmp2;
+	 $returnme .='];';
+	 $returnme .='
+	 ';
+	 $returnme .=' for (var i = 0; i < sites.length; i++) {';
+	 $returnme .=' var site = sites[i];
+	 ';
+   $returnme .=' var siteLatLng = new google.maps.LatLng(site[0], site[1]);
+   ';
+   $returnme .=' if(site[3]!=null) { 
+   ';
+   $returnme .=' var markerimage  = site[3];
+   ';
+   $returnme .=' var marker = new google.maps.Marker({
+   ';
+   $returnme .=' position: siteLatLng,
+   ';
+   $returnme .= ' map: '. $attr['id'].',
+   ';
+   $returnme .= ' icon: markerimage,
+   ';
+   $returnme .= ' html: site[2] });
+   '; 
+   $returnme .=' } else {
+   ';
+   $returnme .=' var marker = new google.maps.Marker({
+   ';
+   $returnme .=' position: siteLatLng,
+   ';
+   $returnme .= ' map: '. $attr['id'].',
+   ';
+   $returnme .= ' html: site[2] });
+   ';
+   $returnme .=' }
+   ';
+   $returnme .= ' var contentString = "Some content";';
+   $returnme .= 'google.maps.event.addListener(marker, "click", function () {
+   ';
+   $returnme .= 'infowindow.setContent(this.html);
+   ';
+   $returnme .= ' infowindow.open('.$attr['id'].', this);
+   ';
+   $returnme .= '});
+   ';
+   $returnme .= '}
+   ';
+ 	   $returnme .=' infowindow = new google.maps.InfoWindow({
+                content: "loading..."
+            });
+    ';
+ 	$returnme .= '</script>';
+		return $returnme;
+	?>
+    
+    
+
+	<?php
+}
+add_shortcode('MultipleMarkerMap', 'MultipleMarkerMap_call');
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,75 @@
+=== Plugin Name ===
+Contributors: soundwaves-productions,yohman
+Tags: google, google maps, google maps, shortcode, shortcodes, google maps v3, v3, geocode, map, multiple marker, marker, mapping, maps, latitude, longitude, api, traffic
+Requires at least: 2.8
+Tested up to: 3.1
+Stable tag: 1.00
+
+This plugin supports multiple markers per map! Kudos to yohman for his plugin:
+http://wordpress.org/extend/plugins/google-maps-v3-shortcode/
+
+
+== Description ==
+
+This plugin allows you to add a google map into your post/page using shortcodes. 
+This plugin allows you to add one or more maps (via the Google Maps v3 API) to your page/post using shortcodes.
+This plugin supports multiple markers per map !
+
+Features:
+
+* multiple maps on the same post
+* multiple markers per map
+* set map size
+* set zoom level
+* set map type
+* set location by latitude/longitude
+* set location by address
+* add marker
+* add custom image as map icon
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload 'google-maps-v3-shortcode-multiple-markers' directory to the '/wp-content/plugins/' directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Add shortcodes in your posts, e.g.
+[MultipleMarkerMap id="MultipleMarkerMapDemo" z="12" lat="48.220162" lon="16.287525" marker="48.193054,16.261282,Bar,http://google-maps-icons.googlecode.com/files/cocktail.png|48.220162,16.287525,point-of-view,http://google-maps-icons.googlecode.com/files/beautiful.png" w="690" h="442"]
+
+== Frequently Asked Questions ==
+= How do I add a map to my post =
+
+Use shortcodes:
+
+e.g.:
+[MultipleMarkerMap id="MultipleMarkerMapDemo" z="12" lat="48.220162" lon="16.287525" marker="48.193054,16.261282,Bar,http://google-maps-icons.googlecode.com/files/cocktail.png|48.220162,16.287525,point-of-view,http://google-maps-icons.googlecode.com/files/beautiful.png" w="690" h="442"]
+
+
+= Can I add multiple maps to the same post? =
+
+Yes!  But make sure you use the "id" parameter to create unique id's for each map.
+
+e.g.:
+[MultipleMarkerMap id="MultipleMarkerMapDemo" z="12" lat="48.220162" lon="16.287525" marker="48.193054,16.261282,Bar,http://google-maps-icons.googlecode.com/files/cocktail.png|48.220162,16.287525,point-of-view,http://google-maps-icons.googlecode.com/files/beautiful.png" w="690" h="442"]
+[MultipleMarkerMap id="MultipleMarkerMapDemo2" z="12" lat="48.220162" lon="16.287525" marker="48.203054,16.261282,Bar,http://google-maps-icons.googlecode.com/files/cocktail.png|48.230162,16.287525,point-of-view,http://google-maps-icons.googlecode.com/files/beautiful.png" w="690" h="442"]
+
+= Can I add multiple markers on the same map? =
+
+Yes ! Just add following shortcode to your page or post (markers are separated with "|" )
+
+[MultipleMarkerMap id="MultipleMarkerMapDemo" z="12" lat="48.220162" lon="16.287525" marker="48.193054,16.261282,Bar,http://google-maps-icons.googlecode.com/files/cocktail.png|48.220162,16.287525,point-of-view,http://google-maps-icons.googlecode.com/files/beautiful.png" w="690" h="442"]
+
+
+= Can I change the size of the map? =
+Yes!  Just add your own width and height parameters (the default is 400x300).
+
+Ex:
+[MultipleMarkerMap w="200" h="100"]
+
+== Changelog ==
+
+= 1.0 =
+* First release
